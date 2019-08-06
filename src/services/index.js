5da794707447c61d2b733b3803ab42d344d84e17
daleran <daleran@gmail.com>
2019-08-06 22:03:39 +0900

    Scaffolded api project

---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+const clientDb = require('./clientDb')
+const email = require('./email')
+const timesheets = require('./timesheets')
+
+module.exports = { clientDb, email, timesheets }
---
+++ src/services/timesheets/toggl.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+// Create a client in Toggle
+const createClient = async (name) => {
+
+}
+
+// Update the client's name in Toggl
+const updateClient = async (name) => {
+
+}
+
+// Get all the time entries that are not marked as invoiced
+const getUninvoicedEntries = async (clientId) => {
+
+}
+
+// Tag all uninvoiced entries as invoiced
+const invoiceEntries = async (clientId) => {
+
+}
+
+// Tag all invoiced entries but not billed entries as billed
+const billEntries = async (clientId) => {
+
+}
