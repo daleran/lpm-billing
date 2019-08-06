daac762a26e80529e53df3e84424b79c3b65ed49
daleran <daleran@gmail.com>
2019-08-06 11:14:12 +0900

    Added repositories

--- src/repositories/invoice.repository.ts
+++ src/repositories/invoice.repository.ts
@@ -1,16 +1,18 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Invoice, InvoiceRelations} from '../models';
-import {DbDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import { Getter, inject } from '@loopback/core';
+import { DefaultCrudRepository, HasManyRepositoryFactory, repository } from '@loopback/repository';
+import { Invoice, InvoiceRelations, LineItem } from '../models';
+import { LineItemRepository } from '../repositories';
+import { DbDataSource } from '../datasources';
 
-export class InvoiceRepository extends DefaultCrudRepository<
-  Invoice,
-  typeof Invoice.prototype.id,
-  InvoiceRelations
-> {
+export class InvoiceRepository extends DefaultCrudRepository<Invoice, typeof Invoice.prototype.id, InvoiceRelations> {
+  public readonly lineItems: HasManyRepositoryFactory<LineItem, typeof Invoice.prototype.id>;
   constructor(
     @inject('datasources.db') dataSource: DbDataSource,
-  ) {
+    @repository.getter(LineItemRepository)
+    protected lineItemRepositoryGetter: Getter<LineItemRepository>, ) {
     super(Invoice, dataSource);
+    this.lineItems = this.createHasManyRepositoryFactoryFor('lineItems', lineItemRepositoryGetter);
   }
+
+
 }
